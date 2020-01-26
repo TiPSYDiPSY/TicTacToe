21188ba1e6d7c863deb9a7a7ca77c35ddf237507
Deniss <sutugin002@gmail.com>
2020-01-26 22:19:33 +0200

    Fixed with out of bound exception

--- main.go
+++ main.go
@@ -1,6 +1,10 @@
 package main
 
+import "TicTacToe/ttt"
 
 func main() {
-
+	hp := ttt.NewHumanPlayer("Deniss", "X")
+	cp := ttt.NewHumanPlayer("AI", "0")
+	g := ttt.NewGame(hp, cp)
+	g.Start()
 }
--- ttt/human.go
+++ ttt/human.go
@@ -1,6 +1,9 @@
 package ttt
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 func NewHumanPlayer(n string, m string) *HumanPlayer {
 	return  &HumanPlayer{
@@ -18,8 +21,14 @@ type HumanPlayer struct {
 func (p *HumanPlayer) GetMove(b *board) (int, int, error) {
 	fmt.Println("Enter position: ")
 	var i,j int
+
 	if n, err := fmt.Scanf("%d %d", &i, &j); err != nil || n != 2 {
 		return 0, 0, err
+	}
+
+	if i > 2 || j > 2 {
+		err := errors.New("position can be only 0,1,2")
+		return  0, 0, err
 	}
 
 	fmt.Println("You input:", i, j)
